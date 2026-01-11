126bb29ea1851c5d37ff4057874c8ac2b943812d
inu <hasibol@blackdevs.co>
2026-01-11 10:30:23 +0600

    no change

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
-import 'package:fair_pro/firebase_options.dart';
+import 'package:fair_pro/services/auth_services.dart';
 import 'package:fair_pro/screen/home_screen.dart';
 import 'package:fair_pro/screen/login_screen.dart';
-import 'package:fair_pro/services/auth_services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(); // Firebase ইনিশিয়ালাইজ
+
   runApp(const MyApp());
 }
 
@@ -20,21 +18,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: StreamBuilder<User?>(
-        stream: AuthServices().authStateChanges,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return HomeScreen();
-          } else {
-            return LoginScreen();
-          }
-        },
-      ),
+      title: 'Your App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const AuthWrapper(), // এখানে AuthWrapper দিলাম
+    );
+  }
+}
+
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: AuthServices().authStateChanges, // Firebase auth state stream
+      builder: (context, snapshot) {
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+
+        if (snapshot.hasData && snapshot.data != null) {
+
+          return const HomeScreen();
+        } else {
+
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
