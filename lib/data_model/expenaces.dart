c9f93c5b49e6b2fa1015eb503870e032cdcb14b9
moobin <moobinislam3@gamil.com>
2026-01-11 22:34:09 +0600

    moduel_12

---
+++ lib/data_model/expenaces.dart
@@ -0,0 +1,12 @@
+class Expenaces {
+   String titile;
+   double amount;
+   DateTime dateTime;
+   String catagory;
+  Expenaces({
+    required this.titile,
+    required this.amount,
+    required this.dateTime,
+    required this.catagory,
+  });
+}
